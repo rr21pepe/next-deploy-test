ccfeed32994c9e73ca7e49a013a17f12125ecbd9
rr21pepe <pepe@redradix.com>
2022-03-08 10:34:48 +0100

    Update Home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ export default function Home() {
     <section>
       <h1>Index page</h1>
       <p>This is the home page</p>
+      <p>Updating Home to test CI</p>
       <Link href="/about">About</Link>
     </section>
   );
