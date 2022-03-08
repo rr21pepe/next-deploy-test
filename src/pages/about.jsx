4ac403564529c82169ee47a99069f88963181471
rr21pepe <pepe@redradix.com>
2022-03-08 10:27:13 +0100

    Update About page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -5,6 +5,7 @@ export default function Contact() {
     <section>
       <h1>About us</h1>
       <p>This is a web to test the Vercel deployment system</p>
+      <p>This is an update to test the CI</p>
       <Link href="/">Come back to home</Link>
     </section>
   );
